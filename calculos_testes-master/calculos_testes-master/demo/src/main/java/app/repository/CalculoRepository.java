9c399a4e7076427c653f8f146ebef2d851e94da2
whiter-wolf <129910876+whiter-wolf@users.noreply.github.com>
2025-06-12 19:11:18 -0300

    Delete calculos_testes-master/calculos_testes-master/demo directory

--- calculos_testes-master/calculos_testes-master/demo/src/main/java/app/repository/CalculoRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.entity.Calculo;
-
-public interface CalculoRepository extends JpaRepository<Calculo, Long>{
-
-}
